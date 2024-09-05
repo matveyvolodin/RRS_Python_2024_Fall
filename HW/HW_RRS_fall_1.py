1d6b9e7c1d79cb785a325fc4c976b917dfd1ef12
matveyvolodin <153534241+matveyvolodin@users.noreply.github.com>
2024-09-05 21:26:16 +0500

    HW_RRS_fall_1 added

---
+++ HW/HW_RRS_fall_1.py
@@ -0,0 +1,20 @@
+# 1.2
+print("Hello World!")
+
+# 1.4
+name = "Matvey"
+print("Hello, " + name)
+print(f"Hello, {name}")
+
+# 1.5
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+print(f"Hello, {name.title()} {surname.title()}")
+
+# 1.6
+a = input("Enter first number to add: ")
+b = input("Enter second number to add: ")
+result = int(a) + int(b)
+print(result)
+
+
