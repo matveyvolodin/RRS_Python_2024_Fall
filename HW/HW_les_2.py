71516cb477630fb2514b57ae3801236118839e97
matveyvolodin <153534241+matveyvolodin@users.noreply.github.com>
2024-09-09 13:33:58 +0500

    HW_les_2.py added

---
+++ HW/HW_les_2.py
@@ -0,0 +1,62 @@
+# # 1 Четное или нечетное
+# n = input("Enter your number: ")
+# if int(n) % 2:
+#     print("Odd")
+# else:
+#     print("Even")
+
+
+# # 2 Какой сегодня день?
+#
+# day = input("Выбери один из дней недели: ")
+# if day == "суббота" or day == "воскресенье":
+#     print("Сегодня выходной!")
+# elif day == "среда":
+#     print("Мне сегодня к стоматологу в 15:30")
+# else:
+#     print("Сегодня обычный день")
+
+
+# # 3 Эхо
+#
+# n = int(input("Введи число"))
+# text = input("Введи строку")
+# while n > 0:
+#     n -= 1
+#     print(text)
+
+
+# # 4 Сколько гласных букв?
+# chars = ""
+# message = input("Введи сообщение")
+# for c in message:
+#     if c in "йуеыаэяиюо":
+#         chars += c
+# print(len(chars))
+
+
+# counter = 0
+# message = input("Введи сообщение")
+# for c in message:
+#     if c in "йуеыаэяиюо":
+#         counter += 1
+# print(counter)
+
+
+# # 5 Сумма чисел
+# nlist = []
+# while True:
+#     num = int(input("Введи число"))
+#     nlist.append(num)
+#     if num < 0:
+#         break
+# print(sum(nlist))
+
+
+# counter = 0
+# while True:
+#     num = int(input("Введи число: "))
+#     counter += num
+#     if num < 0:
+#         break
+# print(counter)
