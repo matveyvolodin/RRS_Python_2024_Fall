1ba3fc2d0a165b0abc321623f8aa73d5828d9da2
matveyvolodin <153534241+matveyvolodin@users.noreply.github.com>
2024-09-10 17:19:34 +0500

    add_task1 solution added

--- Homework/lesson3_solution.py
+++ Homework/lesson3_solution.py
@@ -1,59 +1,71 @@
-# # 1. Особые числа.
-#
-# numbers = []
-#
-# for i in range(101):
-#     if i % 2 == 0 and i % 3 == 0:
-#         numbers.append(i)
-# print(numbers)
-#
-#
-# numbers = [i for i in range(101) if i % 2 == 0 and i % 3 ==0]
-# print(numbers)
+# 1. Особые числа.
+
+numbers = []
+
+for i in range(101):
+    if i % 2 == 0 and i % 3 == 0:
+        numbers.append(i)
+print(numbers)
 
 
-# 2. Фильтр.
-
-# items = [1.2, 3, None, 100, {'info': 'bla-bla'}, 44, 'Hi!', 99, 44.32, None]
-# # Решение при помощи list comprehension
-# numbers = [i for i in items if isinstance(i, (int, float))]
-# print(sum(numbers))
+numbers = [i for i in range(101) if i % 2 == 0 and i % 3 ==0]
+print(numbers)
 
 
-# # 3. История сообщений.
-#
-# messages = []
-# while True:
-#     message = input("Введи свое сообщение: ")
-#     messages.append(message)
-#     if len(messages) > 5:
-#         messages.remove(messages[0])
-#     if message == "пока":
-#         print(f"Ну ладно, пока! {messages}")
-#         break
+2. Фильтр.
+
+items = [1.2, 3, None, 100, {'info': 'bla-bla'}, 44, 'Hi!', 99, 44.32, None]
+# Решение при помощи list comprehension
+numbers = [i for i in items if isinstance(i, (int, float))]
+print(sum(numbers))
+
+
+# 3. История сообщений.
+
+messages = []
+while True:
+    message = input("Введи свое сообщение: ")
+    messages.append(message)
+    if len(messages) > 5:
+        messages.remove(messages[0])
+    if message == "пока":
+        print(f"Ну ладно, пока! {messages}")
+        break
 
 # 4. Без дубликатов.
 
-# # Решение при помощи множества
-# numbers = [15, 3, 8, 42, 3, 23, 8, 7, 42, 1, 9, 23, 15, 5, 9, 4, 6, 1]
-# num_set = set(numbers)
-# sorted_list = sorted(num_set)
-# print(sorted_list)
+# Решение при помощи множества
+numbers = [15, 3, 8, 42, 3, 23, 8, 7, 42, 1, 9, 23, 15, 5, 9, 4, 6, 1]
+num_set = set(numbers)
+sorted_list = sorted(num_set)
+print(sorted_list)
 
-# # Решение при помощи цикла
-# numbers = [15, 3, 8, 42, 3, 23, 8, 7, 42, 1, 9, 23, 15, 5, 9, 4, 6, 1]
-# new_list = []
-# for i in numbers:
-#     if i not in new_list:
-#         new_list.append(i)
-# print(sorted(new_list))
+# Решение при помощи цикла
+numbers = [15, 3, 8, 42, 3, 23, 8, 7, 42, 1, 9, 23, 15, 5, 9, 4, 6, 1]
+new_list = []
+for i in numbers:
+    if i not in new_list:
+        new_list.append(i)
+print(sorted(new_list))
 
-## Дополнительное задание.
+# Дополнительное задание.
 
 # 1. Магазин.
-products = {"apple": {"quantity": 10, "price": 100},
-             "banana": {"quantity": 20, "price": 50},
-             "orange": {"quantity": 15, "price": 80},
-             "grape": {"quantity": 8, "price": 120},
-             "milk": {"quantity": 12, "price": 90},
-             "bread": {"quantity": 30, "price": 40}}
+products = {
+    "apple": {"quantity": 10, "price": 100},
+    "banana": {"quantity": 20, "price": 50},
+    "orange": {"quantity": 15, "price": 80},
+    "grape": {"quantity": 8, "price": 120},
+    "milk": {"quantity": 12, "price": 90},
+    "bread": {"quantity": 30, "price": 40}
+}
+for data in products.values():
+    data["price"] *= 1.2
+
+del (products["milk"])
+products["salt"] = {"quantity": 7, "price": 12}
+
+total_price = 0
+for data in products.values():
+    total_price += data["quantity"] * data["price"]
+print(total_price)
